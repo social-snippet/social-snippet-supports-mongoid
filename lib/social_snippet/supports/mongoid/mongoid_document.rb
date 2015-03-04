884945a5c6e7b3d38c95066224ac5cabf7625474
Hiroyuki Sano <sh19910711@gmail.com>
2015-03-04 11:17:40 +0900

    add MongoidDocument class

---
+++ lib/social_snippet/supports/mongoid/mongoid_document.rb
@@ -0,0 +1,16 @@
+module SocialSnippet::Supports::Mongoid
+
+  class MongoidDocument
+
+    include ::Mongoid::Document
+
+    def self.activate!
+      ::SocialSnippet.class_eval do
+        remove_const :Document if defined?(::SocialSnippet::Document)
+        const_set :Document, MongoidDocument
+      end
+    end
+
+  end
+
+end
